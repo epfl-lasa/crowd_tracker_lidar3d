62a7f9103406071188f1de0f70b086ed4f233aac
Lara Brudermueller <lara.brudermueller@tum.de>
2020-05-07 21:41:17 +0200

    change quaternion values

--- src/crowd_tracker_lidar3d/preprocessing.py
+++ src/crowd_tracker_lidar3d/preprocessing.py
@@ -4,14 +4,15 @@ from scipy.spatial.transform import Rotation as R
 
 def rotate_pcl(data, quat=None): 
     if not quat: 
-        quat = [0, 0.0261769, 0, 0.9996573] # obtained from sensor calibration 
+        # quat = [0, 0.0261769, 0, 0.9996573] # obtained from sensor calibration 
+        quat =  [0, -0.0261769, 0, 0.9996573]
     r = R.from_quat(quat)
     rot_mat = r.as_dcm() # represent rotation as direct cosine matrices (3x3 real orth. mat. with determinant=1)
 
         # R = [  0.9975641,  0.0000000,  0.0697565;
         #        0.0000000,  1.0000000,  0.0000000;
         #       -0.0697565,  0.0000000,  0.9975641 ]
-   
+    print(rot_mat)
     data_array = data[['x','y','z']].to_numpy()
     data_rotated = np.dot(data_array, rot_mat.T)
 
