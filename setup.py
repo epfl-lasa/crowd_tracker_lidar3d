bfd22b904e858b1fa69e55d37ede08ed41d9d575
Lara Brudermueller <lara.brudermueller@tum.de>
2020-09-03 12:15:38 +0200

    Fix catkin setup for catkin_make

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['crowd_tracker_lidar3d'],
     package_dir={'': 'src'},
-    scripts=['scripts/rosbag_reader', 
-             'scripts/file_annotator',
-             'scripts/bounding_box_generator']
+    # scripts=['scripts/rosbag_reader', 
+    #          'scripts/file_annotator',
+    #          'scripts/bounding_box_generator']
 )
 
 setup(**d)
